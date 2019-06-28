43750c7ab617db7efc23abd71ac3c4bb1a06c981
zhouziyu <zhouziyuuuuu@gmail.com>
2019-06-28 10:14:33 +0800

    up

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
 
 Vue.use(Router);
 
@@ -8,16 +7,8 @@ export default new Router({
   routes: [
     {
       path: '/',
+      redirect: '/download',
       name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
     {
       path: '/download',
@@ -25,7 +16,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/download/index.vue'),
+      component: () => import(/* webpackChunkName: "download" */ './views/download/index.vue'),
     },
   ],
 });
